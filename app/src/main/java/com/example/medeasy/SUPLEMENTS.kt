66fab1c99fef0cec879368ffc3d1d4df6e155c72
G Rishika <uni.samiksha@gmail.com>
2024-07-07 20:12:41 +0530

    rishika-7/7/24-1

---
+++ app/src/main/java/com/example/medeasy/SUPLEMENTS.kt
@@ -0,0 +1,20 @@
+package com.example.medeasy
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SUPLEMENTS : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_suplements)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
