5ee4375a060bfe965044724c8438ae6f47169a80
G Rishika <uni.samiksha@gmail.com>
2024-07-08 20:55:43 +0530

    rishika-7/7/24-1

--- app/src/main/java/com/example/medeasy/HomePage.kt
+++ app/src/main/java/com/example/medeasy/HomePage.kt
@@ -14,39 +14,41 @@ class HomePage : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_home_page)
         var button2=findViewById<Button>(R.id.category_1)
-        var button3=findViewById<Button>(R.id.category_2)
-        var button4=findViewById<Button>(R.id.category_3)
         button2.setOnClickListener{
             val intent2 = Intent(this,Lab_test::class.java)
             startActivity(intent2)
-
         }
-        button3.setOnClickListener{
-            val intent2 = Intent(this,products::class.java)
-            startActivity(intent2)
-
-        }
-        button4.setOnClickListener{
-            val intent2 = Intent(this,cart::class.java)
-            startActivity(intent2)
-
-        }
-
-
-       ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.category_1)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.category_1)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        var button3=findViewById<Button>(R.id.category_2)
+        button3.setOnClickListener{
+            val intent2 = Intent(this,products::class.java)
+            startActivity(intent2)}
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.category_2)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        var button4=findViewById<Button>(R.id.category_3)
+        button4.setOnClickListener{
+            val intent2 = Intent(this,cart::class.java)
+            startActivity(intent2)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.category_3)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+
+
+
+
+
+
         }
     }
 }
--- app/src/main/java/com/example/medeasy/Lab_test.kt
+++ app/src/main/java/com/example/medeasy/Lab_test.kt
@@ -1,6 +1,7 @@
 package com.example.medeasy
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +12,11 @@ class Lab_test : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_lab_test)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+//        var button8=findViewById<Button>(R.id.)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
     }
 }
